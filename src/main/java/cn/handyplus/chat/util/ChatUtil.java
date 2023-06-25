ae711ca2a780fdaf6d6ddd286250beca4d4fae1f
handy <handyplus@163.com>
2023-06-25 11:48:59 +0800

    添加权限解析颜色代码，物品处理

--- src/main/java/cn/handyplus/chat/util/ChatUtil.java
+++ src/main/java/cn/handyplus/chat/util/ChatUtil.java
@@ -85,10 +85,9 @@ public class ChatUtil {
         TextUtil playerTextComponent = TextUtil.getInstance().init(playerText);
         playerTextComponent.addHoverText(CollUtil.listToStr(playerHover, "\n"));
         // 消息
-        TextUtil msgTextComponent = null;
+        TextUtil msgTextComponent = TextUtil.getInstance().init(msgText + msg.getMessage());
         // 聊天处理
         if (ChatConstants.CHAT_TYPE.equals(msg.getType())) {
-            msgTextComponent = TextUtil.getInstance().init(msgText + msg.getMessage());
             msgTextComponent.addHoverText(CollUtil.listToStr(msgHover, "\n"));
         }
         // 物品展示处理
