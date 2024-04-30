7d927bfdcc60b9dafdc9f2786ae864ae10e021da
handy <handyplus@163.com>
2024-04-30 15:58:22 +0800

    添加提醒

--- src/main/java/cn/handyplus/chat/command/player/ItemCommand.java
+++ src/main/java/cn/handyplus/chat/command/player/ItemCommand.java
@@ -1,15 +1,20 @@
 package cn.handyplus.chat.command.player;
 
+import cn.handyplus.chat.enter.ChatPlayerItemEnter;
 import cn.handyplus.chat.inventory.ItemGui;
+import cn.handyplus.chat.service.ChatPlayerItemService;
 import cn.handyplus.chat.util.ConfigUtil;
 import cn.handyplus.lib.command.IHandyCommandEvent;
 import cn.handyplus.lib.expand.adapter.HandySchedulerUtil;
 import cn.handyplus.lib.util.AssertUtil;
 import cn.handyplus.lib.util.BaseUtil;
+import cn.handyplus.lib.util.MessageUtil;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+
+import java.util.Optional;
 
 /**
  * 展示物品
@@ -41,6 +46,11 @@ public class ItemCommand implements IHandyCommandEvent {
         Player player = AssertUtil.notPlayer(sender, BaseUtil.getMsgNotColor("noPlayerFailureMsg"));
         // 展示物品ID
         Integer itemId = AssertUtil.isNumericToInt(args[1], sender, BaseUtil.getMsgNotColor("amountFailureMsg"));
+        Optional<ChatPlayerItemEnter> chatPlayerItemOptional = ChatPlayerItemService.getInstance().findById(itemId);
+        if (!chatPlayerItemOptional.isPresent()) {
+            MessageUtil.sendMessage(sender, BaseUtil.getMsgNotColor("itemNotFoundMsg"));
+            return;
+        }
         Inventory inventory = ItemGui.getInstance().createGui(player, itemId);
         HandySchedulerUtil.runTask(() -> player.openInventory(inventory));
     }
