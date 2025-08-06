b9c0a9d3e750ca4e99cdd3deb04740b4226e03dd
handy <handyplus@163.com>
2025-08-06 17:59:27 +0800

    1.3.6 私信简化

---
+++ src/main/java/cn/handyplus/chat/listener/PlayerCommandPreprocessEventListener.java
@@ -0,0 +1,42 @@
+package cn.handyplus.chat.listener;
+
+import cn.handyplus.lib.annotation.HandyListener;
+import cn.handyplus.lib.constants.BaseConstants;
+import cn.handyplus.lib.core.CollUtil;
+import cn.handyplus.lib.expand.adapter.PlayerSchedulerUtil;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+import java.util.List;
+
+/**
+ * 私信简化
+ *
+ * @author handy
+ * @since 1.3.6
+ */
+@HandyListener
+public class PlayerCommandPreprocessEventListener implements Listener {
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onEvent(PlayerCommandPreprocessEvent event) {
+        if (event.isCancelled()) {
+            return;
+        }
+        List<String> tellAlias = BaseConstants.CONFIG.getStringList("tellAlias");
+        if (CollUtil.isEmpty(tellAlias)) {
+            return;
+        }
+        for (String alias : tellAlias) {
+            if (event.getMessage().startsWith(alias)) {
+                String replace = event.getMessage().replace(alias, "/plc tell");
+                PlayerSchedulerUtil.performCommand(event.getPlayer(), replace);
+                event.setCancelled(true);
+                return;
+            }
+        }
+    }
+
+}
